54e1bd27805acf757396e38c518740d41f45df28
FaridAghazade <gunay.agayeva01@gmail.com>
2026-01-16 22:07:18 +0100

    tapshiriq60!!!!

--- ders58/variable.js
+++ ders58/variable.js
@@ -1,24 +1,44 @@
-let sum = " menim adim Murad";
-let sal = "salam";
-let sag = "sagol";
-let full = `Bu full bir cumle dir. ${sal} ${sum}`
-let trim = "           mmms               "
 
-console.log(full );
-console.log(sum.length)
-console.log(sum[0]);//niye 0-rinciny gorsedmir
-console.log(full.indexOf("bir"));
-console.log(full.lastIndexOf("B"));
-console.log( sal.concat( " ").concat(sag));
-console.log(sum.slice());//men burda ele eliye bilerem ki men bunun herif sayini bilmirem ama elebir adimi butov axrxadan gorsedsin
-console.log('murad\' Home  ');
-console.log(sag.substring(2,3));
-console.log(sum.replaceAll("e","o"));
-console.log(full.replaceAll("l","i"));
-console.log(sag.toUpperCase());
-console.log(sag.toLowerCase());
-console.log(sum.charAt(length-1));//bes bele niye olmur
-console.log(trim.trim());
+// let sum = "menim adim Murad";
+// let sal = "salam";
+
+// let sag = "sagol";
+// let full = `Bu full bir cumle dir. ${sal} ${sum}`
+// let trim = "           mmms               "
+
+// console.log(full);
+// console.log(sum.length)
+// console.log(sum[0]);
+// console.log(full.indexOf("bir"));
+//  console.log(full.lastIndexOf("B"));
+//  console.log( sal.concat( " ").concat(sag));
+//  console.log(sum.slice(-5));
+
+
+
+//  console.log('murad\' Home  ');
+
+// console.log(sag.substring(2, 3));
+
+// console.log(sum.replaceAll("e", "o"));
+// console.log(full.replaceAll("l", "i"));
+// console.log(sag.toUpperCase());
+// console.log(sag.toLowerCase());
+// console.log(sum.charAt(sum.length - 1));
+// console.log(trim.trim());
+
+// // let a = "menim web siteme xosh geldiniz"
+
+// // console.warn("attention");
+// // console.error("this variable is undefind");
+
+// // console.clear()
+// // console.log(a);
+
+let name = prompt("Adinizi daxil edin")
+console.log(name);
+let age = prompt("Yashinizi daxil edin")
+console.log(age);
 
 
 
---
+++ ders59/js.js
@@ -0,0 +1,13 @@
+//tapshiriq 1
+let num1= +prompt("1-rinci ededi daxil edin ama birinci eded ikinci ededen balaca olmasin ")
+let num2= +prompt("2-rinci ededi daxil edin ama  ikinci eded birinci ededen böyük olmasin")
+
+console.log(num1 + num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(num1 - num2 );
+
+
+//tapshiriq 2
+let name = prompt("Adinizi daxil edin");
+console.log(`Salam, ${name} `);

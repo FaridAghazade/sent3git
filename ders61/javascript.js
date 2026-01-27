dd7f8da9d2ee7fa331bc3cc088ff7d1c770b2ad7
FaridAghazade <gunay.agayeva01@gmail.com>
2026-01-27 12:40:47 +0100

    ders62

---
+++ ders61/javascript.js
@@ -0,0 +1,50 @@
+// let month =+prompt("hansisa ayi daxil edin" )
+// switch (month) {
+//     case 1:
+//         console.log("Janvar");
+//         break;
+//         case 2:
+//             console.log("Fevral");
+//             break;
+//         case 3:
+//             console.log("Mart");
+//             break;
+//         case 4:
+//             console.log("Aprel");
+//             break;
+//         case 5:
+//             console.log("May");
+//             break;
+//         case 6:
+//             console.log("Iyun");
+//             break;
+//             default:
+//                 console.log("bele bir ay yoxdur");
+// }
+// let id = prompt("mehsul id daxil edin:")
+// switch (id) {
+//     case "1":
+//         alert("Movcud 10 eded.")
+//     case "1":
+//         alert("Movcud 3 eded.")
+//         break;
+
+//     case "2":
+//         alert("Movcud 256 eded.")
+//         break;
+//     case "1":
+//         alert("Movcud 53 eded.")
+//         break;
+
+        
+
+
+
+//     default:
+//         alert("Movcud deyil")
+//         break;
+// }
+let sual =prompt("cinsiniz nedir M yoxsa F")
+let gender=sual.toUpperCase() =="M" ? "Male" :"Female";
+console.log(gender);
+
---
+++ ders62/script.js
@@ -0,0 +1,51 @@
+// let number = confirm("nomreniz Cüt-durse onda ok basin eks halda cancel basin");
+// let all = number==true? 'Nomreniz cüt-dür' : "Nomreniz Tək-dir" ;
+// console.log(all);
+// //tap1
+// let price =confirm("aldiqiniz Koftanin qiymeti 100-den boyuk-durse onda ok basin eks halda cancel ")
+// let endirim = price>100 ? "Endirim var":"endirim yoxdur"
+// console.log(endirim);
+// //tap2
+// let color =prompt("choose a color")
+// switch (color) {
+//     case "red":
+//         console.log( "Qırmızı seçildi");
+//         break;
+// case "blue":
+//     console.log( "Mavi seçildi");
+//     break;
+//     case "green":
+//         console.log("Yaşıl seçildi");
+            
+//     default:
+//         console.log("bele bir reng yoxdur");
+        
+//         break;
+// }
+//tap3
+let a = 10;
+
+let b = 5;
+let operator =prompt("hansisa riyazi operator daxil edin");
+
+switch (operator) {
+    case "+" :
+        console.log(a + b);
+        break;
+        
+    case "-" :
+        console.log(a - b);
+        break;
+    case "/" :
+        console.log(a / b);
+        break;
+    case "*" :
+        console.log(a * b);
+        break;
+
+
+    default:
+        console.log("belebir hesablama emeliyati yoxdur");
+        
+        break;
+}
